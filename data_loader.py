a33dc5d38c29da6cd3843bd10584221490133949
Aranat404 <daniel.loading404@gmail.com>
2025-06-01 18:23:11 -0300

    docs: Documentando funções da Main e data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,22 +1,58 @@
+# Parte responsável em lidar com os dados (preparar, carregar) do MNIST.
+# Normalização dos dados, gerenciamento dos arquivos compactados,
+# gerencia dos dados das imagens e dos rótulos.
+#
+# Explicação dos imports:
+# gzip = para lidar com os arquivos compactados em .gz
+# os = para lidar com caminhos dos arquivos
+# struct = interpretação dos arquivos MNIST
+# numpy = para lidar com manipulação de arrays e outras operações numéricas
 import gzip
-import numpy as np
 import os
 import struct
+import numpy as np
 
+
+# Função para carregar e normalizar os dados:
+# Usando o gzip, irá abrir o arquivo como leitura binária.
+# Usando o struct, irá ler os 16 primeiros bytes do cabeçalho do arquivo (f.read(16))
+# >IIII significa ler 4 inteiros de 32 bits (4 bytes cada) no formato Big Endian.
+# O Big Endian é uma maneira de ler do byte mais importante pro menos importante.
+# Os 16 primeiros bytes são identificadores nos arquivos MNIST, sendo:
+# 0 ao 3 = identificador do arquivo (_ para ignorar)
+# 4 ao 7 = número de imagens (num_imagens)
+# 8 ao 11 = número de linhas (linhas)
+# 12 ao 15 = número de colunas (colunas)
+# sendo eles: identificador do arquivo, número de imagens, linhas e colunas.
+# Realiza a leitura dos pixels das imagens e converte isso pra um array numpy.
+# Após isso, usando o reshape, se tem um array com os pixels e agora separa nos valores
+# para imagens individuais (pixels da imagem, valor de linhas e colunas de forma linear).
+# Nomraliza os dados convertendo para valores em float de 0 a 255.
 def carregar_imagens(caminho_arquivo):
     with gzip.open(caminho_arquivo, 'rb') as f:
-        magic, num_imagens, linhas, colunas = struct.unpack(">IIII", f.read(16))
+        _, num_imagens, linhas, colunas = struct.unpack(">IIII", f.read(16))
         imagens = np.frombuffer(f.read(), dtype=np.uint8)
         imagens = imagens.reshape(num_imagens, linhas * colunas)
         imagens = imagens.astype(np.float32) / 255.0
         return imagens
 
+# Agora realizando a leitura dos rótulos:
+# Novamente fazendo uma leitura binária no arquivo
+# Realiza a leitura do arquivo e ignora
+# o cabeçalho e o número de rótulos nos 8 primeiros bytes.
+# Após isso, cria um array numpy com os índices de cada imagem (rótulo referente a imagem)
+# retorna esse array com esses rótulos referentes as imagens.
 def carregar_rotulos(caminho_arquivo):
     with gzip.open(caminho_arquivo, 'rb') as f:
-        magic, num_rotulos = struct.unpack(">II", f.read(8))
+        _, _ = struct.unpack(">II", f.read(8))
         rotulos = np.frombuffer(f.read(), dtype=np.uint8)
         return rotulos
 
+# Carregando os arquivos de imagem:
+# primeiro padroniza pra vários sistemas o caminho da pasta
+# passa pra dentro de cada variável os arrays das funções de rótulos e imagens
+# montadas acima.
+# retornas essas variáveis pra iniciar o treino.
 def carregar_dados_mnist(pasta="dados"):
     X_train = carregar_imagens(os.path.join(pasta, 'train-images-idx3-ubyte.gz'))
     y_train = carregar_rotulos(os.path.join(pasta, 'train-labels-idx1-ubyte.gz'))
--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-from mlp import MLP
-from data_loader import carregar_dados_mnist
+# Parte principal do projeto responsável por:
+# iniciar o treino, chamar os dados e criar as matrizes
+#
+# Explicação dos imports:
+# avaliacao = importando do módulo de avaliação as funções estruturadas de acurácia e matriz.
+# data_loader = importanto do módulo de carregar os dados a função de carregar os arquivos.
+# MLP = importanto a classe MLP do módulo do Multilayer Perceptron.
+# matplotlib.pyplot = importando biblioteca pra ajudar a plotar os dados gerados pela rede.
 from avaliacao import calcular_acuracia, matriz_confusao, exibir_matriz_confusao
+from data_loader import carregar_dados_mnist
+from mlp import MLP
+
 import matplotlib.pyplot as plt
 
-# Carrega os dados
+
+# Carrega os dados dos arquivos do MNIST
 X_train, y_train, X_test, y_test = carregar_dados_mnist()
 
-# Instancia e treina a rede
+# Inicializa a rede e passa os padrões de
+# taxa de tamanho da camada oculta e taxa de aprendizado
+# recebe o histórico de perdas conforme o passar das épocas
 modelo = MLP(hidden_size=128, taxa_aprendizado=0.1)
-historico = modelo.treinar(X_train, y_train, 5)
+historico = modelo.treinar(X_train, y_train, 80)
 
-# Avaliação
+# Faz o cálculo da precisão dos testes da
+# rede com o passar das épocas com 4 casas de precisão
 pred_test = modelo.forward(X_test)
 acc = calcular_acuracia(pred_test, y_test)
 print(f"Acurácia no teste: {acc:.4f}")
 
-# Matriz de confusão
+# Monta uma matriz de confusão 10x10
+# comparando os valores reais dos valores preditos
+# por fim, exibe a matriz de confusão.
 y_predito = pred_test.argmax(axis=1)
 matriz = matriz_confusao(y_test, y_predito)
 exibir_matriz_confusao(matriz)
 
-# Gráfico da função de perda
+# Estrutura o gráfico no matplot e exibe ele por fim
+# exibe o gráfico baseado no histórico montado durante
+# as épocas de treinamento da rede.
 plt.plot(historico)
 plt.title("Função de Perda por Época")
 plt.xlabel("Época")
